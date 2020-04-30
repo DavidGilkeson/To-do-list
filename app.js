1357682d255984ca48c0058b73b99a0df0dc63a3
David Gilkeson <davidgilkeson8@gmail.com>
2020-04-30 01:48:50 +0100

    updated mongo db

--- app.js
+++ app.js
@@ -12,9 +12,12 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  "mongodb+srv://admin-David:Test123@cluster0-wu0im.mongodb.net/todolistDB",
+  {
+    useNewUrlParser: true,
+  }
+);
 
 const itemsSchema = {
   name: String,
